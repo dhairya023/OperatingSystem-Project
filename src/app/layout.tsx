6c2a3712d0b0b6d51a5044dedee59d3aa7378144
Dhairya Patel <dhairyapatel012@gmail.com>
2025-11-03 18:34:49 +0000

    extend the components to remove this blank space

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
                 </SidebarContent>
               </Sidebar>
               <SidebarInset>
-                <main className="p-4 sm:p-6 md:p-8">
+                <main className="p-4 md:p-6 lg:p-8">
                   {children}
                 </main>
               </SidebarInset>
