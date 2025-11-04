e0bc6dee2963dffb42c25366b6dea7d3e5e8b42a
Dhairya Patel <dhairyapatel012@gmail.com>
2025-11-04 09:03:57 +0000

    blank space in mobile screen layout

--- src/components/dashboard/attendance-overview.tsx
+++ src/components/dashboard/attendance-overview.tsx
@@ -18,15 +18,15 @@ import { useAppContext } from "@/context/app-context"
 export default function AttendanceOverview() {
   const { classes } = useAppContext();
   return (
-    <Card className="flex flex-col h-full hover:border-primary/50 transition-colors">
+    <Card className="flex flex-col hover:border-primary/50 transition-colors">
       <CardHeader className="p-4">
         <CardTitle>Attendance</CardTitle>
         <CardDescription>A quick monthly look.</CardDescription>
       </CardHeader>
-      <CardContent className="flex-1 flex items-center justify-center pb-0 p-4 pt-0">
+      <CardContent className="flex items-center justify-center pb-0 p-4 pt-0">
         <SubjectAttendanceCalendar classes={classes} isMini={true} />
       </CardContent>
-      <CardFooter className="flex-col gap-2 pt-4 px-4 pb-4">
+      <CardFooter className="flex-col gap-2 pt-4 px-4 pb-4 mt-auto">
         <Button variant="outline" className="w-full h-9" asChild>
             <Link href="/attendance">
                 Details <ArrowRight className="ml-2 w-4 h-4" />
