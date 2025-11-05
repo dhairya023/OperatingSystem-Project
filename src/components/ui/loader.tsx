0c17bca346e3310a40e7d47113dbbb101d18039c
Dhairya Patel <dhairyapatel012@gmail.com>
2025-11-05 15:22:12 +0000

    this ui skeleton is adding too much load time

---
+++ src/components/ui/loader.tsx
@@ -0,0 +1,13 @@
+
+import { cn } from "@/lib/utils";
+
+export const Loader = ({ className }: { className?: string }) => {
+  return (
+    <div
+      className={cn(
+        "h-12 w-12 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent",
+        className
+      )}
+    ></div>
+  );
+};
